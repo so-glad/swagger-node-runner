25f042216cd26cf299fc0f1999a4264e88793c68
Cartoon Zhang <palmtale@glad.so>
2017-05-09 18:46:09 +0800

    Since included publish registry, dropped dist in git.

--- dist/fittings/swagger_cors.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _cors = require('./cors');
-
-var _cors2 = _interopRequireDefault(_cors);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = _cors2.default;
